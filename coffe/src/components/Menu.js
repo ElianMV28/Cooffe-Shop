29d44c9e77ad8b3bae1a6135d4bce58ac4946d93
Elian Vergara <elianv@bluetrailsoft.com>
2021-11-18 16:10:10 -0300

    start

---
+++ coffe/src/components/Menu.js
@@ -0,0 +1,22 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import './Menu.css';
+import React from "react";
+
+function Menu() {
+  return (
+    <div>
+      <Navbar bg="danger" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Menu;

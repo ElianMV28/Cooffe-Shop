1220371133d31667a73f3e20a529d9e25cddfc02
JuanArias <juanar@bluetrailsoft.com>
2021-11-20 09:39:09 -0300

    add new function a file shop

--- coffe/src/pages/Home/Home.js
+++ coffe/src/pages/Home/Home.js
@@ -9,6 +9,7 @@ export function Home() {
       <Menu />
       <h3>Home Page</h3>
       {/* <Carousel/> */}
+      <div>Iconos diseñados por <a href="https://www.flaticon.es/autores/andrean-prabowo" title="Andrean Prabowo">Andrean Prabowo</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>
     </>
   );
 }
--- coffe/src/pages/Shop/Shop.js
+++ coffe/src/pages/Shop/Shop.js
@@ -1,24 +1,49 @@
-import React from 'react'
-import { Table } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Table, Button } from 'react-bootstrap'
+import { Menu } from '../../components'
 import './Shop.css'
 
 const store = [
     {Name:'French fries', price:125},
-   {Name:'cafe', price:50},
-    {Name:'cortado', price:55},
-    {Name:'capuccino', price:60},
-   {Name:'te', price:50},
-    {Name:'agua', price:80},
-    {Name:'gaseosa', price:85},
-    {Name:'sanwhiches', price:250},
-    {Name:'chocolate', price:150}]
+   {Name:'Cafe', price:50},
+    {Name:'Cortado', price:55},
+    {Name:'Capuccino', price:60},
+   {Name:'Te', price:50},
+    {Name:'Agua', price:80},
+    {Name:'Gaseosa', price:85},
+    {Name:'Sanwhiches', price:250},
+    {Name:'Chocolate', price:150}]
+const sale = {price:100}
 
 
-
+const initialState = false;
+const initialStateTotal = 0;
+const initialStateList = []
 
 export function Shop() {
+    const [ShowTotal, setShowTotal] = useState(initialState)
+    const [check, setCheck] = useState(initialStateTotal)
+    const [listBuy, setListBuy] = useState(initialStateList)
+
+
+    const handleOnClick = (element) => {
+        setListBuy([...listBuy,element])   
+    }
+    const listTotal = listBuy.map(produc => {
+        if(produc.price < sale.price) return produc;
+
+        return {
+            ...produc,
+            price:produc.price -(produc.price * 0.1)
+        };
+    })
+
+    console.log(listTotal)
     return (
+        
         <>
+        <Menu/>
+        <div className='contentTable'>
         <div className='tableShop'>
            <h1>Cofee Shopp</h1>
     <Table responsive="sm">
@@ -26,20 +51,27 @@ export function Shop() {
         <tr>
             <th>Stock</th>
             <th>Price</th>
-            
+            <th>
+            </th>
             
         </tr>
         </thead>
         <tbody>
-        {store.map((elemento) => (
+        {store.map(element => (
             <tr>
-                <th>{elemento.Name}</th>
-                <th>{elemento.price}</th>
+                <th>{element.Name}</th>
+                <th>${element.price}</th>
+                <th>
+            <Button variant="outline-primary">
+                <img src="./assets/agregar-carrito.png" alt="my image" className='buttonImage' onClick={()=>handleOnClick(element)} />
+                </Button>{' '}
+            </th>
             </tr>
         ))}
         
         </tbody>
     </Table> 
+        </div>
         </div>
         </>
     )
